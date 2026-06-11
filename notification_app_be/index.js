16057ef7491db1393f6c472c3e243b65953ba2c0
Vijitashva Narayan Pandey <152644579+dead0005@users.noreply.github.com>
2026-06-11 14:21:39 +0530

    Implemented priority based notification ranking

--- notification_app_be/index.js
+++ notification_app_be/index.js
@@ -13,7 +13,33 @@ Authorization:`Bearer ${process.env.TOKEN}`
 }
 );
 
-console.log(res.data);
+const notifications=res.data.notifications;
+
+const priority={
+Placement:3,
+Result:2,
+Event:1
+};
+
+notifications.sort((a,b)=>{
+
+if(priority[b.Type]!==priority[a.Type]){
+return priority[b.Type]-priority[a.Type];
+}
+
+return new Date(b.Timestamp)-new Date(a.Timestamp);
+
+});
+
+const top10=notifications.slice(0,10);
+
+console.log("TOP 10 NOTIFICATIONS\n");
+
+top10.forEach((n,i)=>{
+console.log(
+`${i+1}. [${n.Type}] ${n.Message} (${n.Timestamp})`
+);
+});
 
 }catch(err){
 console.log(err.response?.data||err.message);
